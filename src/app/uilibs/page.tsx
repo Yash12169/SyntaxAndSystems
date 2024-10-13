bfa9b8d4291446406eabf212c4a392d07e42d0e9
yash <jewalkaryash@gmail.com>
2024-10-13 18:23:27 +0530

    feat(UI): improved page structure

--- src/app/uilibs/page.tsx
+++ src/app/uilibs/page.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layout/layout";
 import Navbar from "@/components/Navbar/Navbar";
 import Starter from "@/components/starter/Starter";
-import { ReactLenis } from '@studio-freight/react-lenis'
+// import { ReactLenis } from '@studio-freight/react-lenis'
 
 export default function Page() {
   return (
